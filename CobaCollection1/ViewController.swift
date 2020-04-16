698985e5eef80ce70a968e11b4230c5d1045efb3
R. Kukuh <rkukuh@gmail.com>
2020-04-16 11:37:01 +0700

    Initial Commit

---
+++ CobaCollection1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CobaCollection1
+//
+//  Created by R. Kukuh on 16/04/20.
+//  Copyright © 2020 R. Kukuh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
