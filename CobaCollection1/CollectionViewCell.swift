6ad2601d49a66f0d0f3e5d5da1a484ac58de51a6
R. Kukuh <rkukuh@gmail.com>
2020-04-20 13:47:16 +0700

    tes

---
+++ CobaCollection1/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CobaCollection1
+//
+//  Created by R. Kukuh on 16/04/20.
+//  Copyright © 2020 R. Kukuh. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var aaa: UIImageView!
+}
